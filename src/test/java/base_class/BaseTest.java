f6957942c5426250a00d9c3e0991609c5d76793a
IMRAN AL MUNYEEM <ialmunyeem@gmail.com>
2021-08-27 21:02:01 +0600

    27th August

--- src/test/java/base_class/BaseTest.java
+++ src/test/java/base_class/BaseTest.java
@@ -1,6 +1,6 @@
 package base_class;
-import org.junit.After;
-import org.junit.Before;
+//import org.junit.After;
+//import org.junit.Before;
 import org.openqa.selenium.By;
 //import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,14 +9,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-//import org.testng.annotations.Test;
+//import org.testng.annotations.AfterSuite;
+//import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 	
 	public static WebDriver driver = null;
-	
-	@Before
+	@Test
+	@BeforeSuite
 	public void launchBrowser() {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
@@ -49,7 +51,7 @@ public class BaseTest {
 		Thread.sleep(2000);
 	}
 	
-	@After
+	@AfterSuite
 	public void closeBrowser() {
 		driver.close();
 	}
