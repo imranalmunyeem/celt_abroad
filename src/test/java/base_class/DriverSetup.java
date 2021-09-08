64e257f8369beeeeec4ed79da07b6c65a1071f66
IMRAN AL MUNYEEM <ialmunyeem@gmail.com>
2021-09-08 19:22:13 +0600

    8th September, TestNG report

--- src/test/java/base_class/DriverSetup.java
+++ src/test/java/base_class/DriverSetup.java
@@ -1,18 +1,9 @@
 package base_class;
-//import org.junit.After;
-//import org.junit.Before;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-//import org.testng.annotations.AfterSuite;
-//import org.testng.annotations.BeforeSuite;
-//import org.testng.annotations.Test;
-///import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class DriverSetup {
 	
